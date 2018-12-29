e927f793f6539874b9718616a9844a00c81bbac6
Hafiz Muhammad Luqman <hafiz_luqman@mentor.com>
2018-12-29 17:08:44 +0100

    Update: update function updated

--- update.m
+++ update.m
@@ -5,16 +5,27 @@
 % Predicted State : x_k_1
 % Predicted Process Covariance Matrix : P_k_1
 
-function [x_k_k, P_k_k, y_k_k] = update(x_k_1, z_k, R_k, P_k_1, H_Sensor_)
-    
-    y_k = z_k' - H_Sensor_*x_k_1;
-    S_k = R_k + H_Sensor_*P_k_1*transpose(H_Sensor_);
-    % Kalman Gain calculation
-    K_k = P_k_1*transpose(H_Sensor_)*inv(S_k);
-    % Posteriro Estimation
-    x_k_k = x_k_1 + K_k*y_k;
-    % State Covariance Matrix Calculation
-    P_k_k = (eye(4,4) - K_k*H_Sensor_) * P_k_1 * transpose((eye(4,4)-K_k*H_Sensor_)) + K_k*R_k*transpose(K_k);
-    y_k_k = z_k' - H_Sensor_*x_k_k;
-    
+function [x_k_k, P_k_k, y_k_k] = update(x_k_1, z_k, P_k_1, R_k, H_Sensor_)
+    if (length(z_k) == 3)
+        z_pred = RadarCartesianToPolar(x_k_1);
+        y_k = z_k' - z_pred;        
+        S_k = R_k + H_Sensor_*P_k_1*transpose(H_Sensor_);
+        % Kalman Gain calculation
+        K_k = P_k_1*transpose(H_Sensor_)*inv(S_k);
+        % Posteriro Estimation
+        x_k_k = x_k_1 + K_k*y_k;
+        % State Covariance Matrix Calculation
+        P_k_k = (eye(4,4) - K_k*H_Sensor_) * P_k_1 * transpose((eye(4,4)-K_k*H_Sensor_)) + K_k*R_k*transpose(K_k);
+        y_k_k = z_k' - H_Sensor_*x_k_k;
+    else
+        y_k = z_k' - H_Sensor_*x_k_1;
+        S_k = R_k + H_Sensor_*P_k_1*transpose(H_Sensor_);
+        % Kalman Gain calculation
+        K_k = P_k_1*transpose(H_Sensor_)*inv(S_k);
+        % Posteriro Estimation
+        x_k_k = x_k_1 + K_k*y_k;
+        % State Covariance Matrix Calculation
+        P_k_k = (eye(4,4) - K_k*H_Sensor_) * P_k_1 * transpose((eye(4,4)-K_k*H_Sensor_)) + K_k*R_k*transpose(K_k);
+        y_k_k = z_k' - H_Sensor_*x_k_k;
+    end
 end
